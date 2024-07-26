81f03311add65baef4ed13314e16d6b23aecb65e
Gzh0821 <gaozih0821@outlook.com>
2024-07-27 00:21:36 +0800

    chore: Update font families in index.scss and theme.ts

---
+++ src/.vuepress/navbar/ru-RU.ts
@@ -0,0 +1,37 @@
+import { navbar } from "vuepress-theme-hope";
+
+export const ruNavbar = navbar([
+  "/ru-RU/",
+  "/ru-RU/download/",
+  "/ru-RU/guide/",
+  "/ru-RU/instructions/",
+  "/ru-RU/contribution/",
+  { text: "Онлайн-игра", link: "https://pvz2-test.gaozih.com", icon: "circle-play" },
+  // "/",
+  // "/portfolio",
+  // "/demo/",
+  // {
+  //   text: "Guide",
+  //   icon: "lightbulb",
+  //   prefix: "/guide/",
+  //   children: [
+  //     {
+  //       text: "Bar",
+  //       icon: "lightbulb",
+  //       prefix: "bar/",
+  //       children: ["baz", { text: "...", icon: "ellipsis", link: "#" }],
+  //     },
+  //     {
+  //       text: "Foo",
+  //       icon: "lightbulb",
+  //       prefix: "foo/",
+  //       children: ["ray", { text: "...", icon: "ellipsis", link: "#" }],
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: "V2 Docs",
+  //   icon: "book",
+  //   link: "https://theme-hope.vuejs.press/",
+  // },
+]);
