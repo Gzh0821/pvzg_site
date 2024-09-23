f8a5f0e728d90dea362cb6846d27b25cb33a983a
Gzh0821 <gaozih0821@outlook.com>
2024-09-23 17:41:16 +0800

    Refactor VuePress config to update gameinfo.json with new features and download links

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -24,17 +24,17 @@ export default defineUserConfig({
     "/en/": {
       lang: "en-US",
       title: "PvZ2 Gardendless Official Website|A completely remastered PVZ2 experience",
-      description: "Official website of PvZ2 Gardendless. PVZ2 Gardendless is a complete remake of Plants vs Zombies 2.",
+      description: "Official website of PvZ2 Gardendless. PVZ2 Gardendless is a complete remastered, full PC platform of Plants vs Zombies 2.",
     },
     "/ru-RU/": {
       lang: "ru-RU",
       title: "PvZ2 Gardendless Официальный сайт|Полностью переработанный опыт PVZ2",
-      description: "Официальный сайт PvZ2 Gardendless. PVZ2 Gardendless - это полная переработка Plants vs Zombies 2.",
+      description: "«PVZ2 Gardendless» — это полностью переделанная платформенная игра «Plants vs Zombies 2» (Plants vs Zombies 2) для ПК.",
     },
     "/": {
       lang: "zh-CN",
       title: "PvZ2 Gardendless 官方网站|完全重置的PVZ2体验",
-      description: "《PvZ2 Gardendless》的官方网站。《PVZ2 Gardendless》是一款完全重制的《植物大战僵尸2》(Plants vs Zombies 2)游戏。",
+      description: "《PvZ2 Gardendless》的官方网站。《PVZ2 Gardendless》是一款完全重制的，全PC平台的《植物大战僵尸2》(Plants vs Zombies 2)游戏。",
     },
   },
 
