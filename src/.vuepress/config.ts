8395d78a9f801b31970358bceb09256721049777
Gzh0821 <gaozih0821@outlook.com>
2025-05-28 00:27:45 +0800

    update version 0.3.4

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -30,22 +30,22 @@ export default defineUserConfig({
   locales: {
     "/en/": {
       lang: "en-US",
-      title: "PvZ2 Gardendless Website",
+      title: "PvZ2 Gardendless Official Website",
       description: "Website of PvZ2 Gardendless. PVZ2 Gardendless is a complete remastered, full PC platform of Plants vs. Zombies 2.",
     },
     "/ru-RU/": {
       lang: "ru-RU",
-      title: "PvZ2 Gardendless сайт",
+      title: "PvZ2 Gardendless Официальный сайт",
       description: "«PVZ2 Gardendless» — это полностью переделанная платформенная игра Plants vs. Zombies 2 для ПК.",
     },
     "/es-ES/": {
       lang: "es-ES",
-      title: "Sitio web de PvZ2 Gardendless",
+      title: "Sitio web oficial de PvZ2 Gardendless",
       description: "Sitio web de PvZ2 Gardendless. PVZ2 Gardendless es una versión remasterizada y completa para PC de Plants vs. Zombies 2.",
     },
     "/": {
       lang: "zh-CN",
-      title: "PvZ2 Gardendless 网站",
+      title: "PvZ2 Gardendless 官方网站",
       description: "《PvZ2 Gardendless》网站。《PVZ2 Gardendless》是一款完全重制的，全PC平台的《植物大战僵尸2》(Plants vs Zombies 2)游戏。",
     },
   },
